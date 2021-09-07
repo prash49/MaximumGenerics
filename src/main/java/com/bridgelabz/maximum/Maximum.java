63ec6ae36594c140acdb146d35888fd73abe89cd
Prashanth <imprash4u@gmail.com>
2021-09-07 18:35:29 +0530

    Added: Welcome message and Program Structure

---
+++ src/main/java/com/bridgelabz/maximum/Maximum.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.maximum;
+
+/**
+ * Hello world!
+ *
+ */
+public class Maximum
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Find Maximum in 3 values Using Generics" );
+    }
+}
