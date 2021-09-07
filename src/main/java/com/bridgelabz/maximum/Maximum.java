d610ffa8205a06f4adb51da03a707a9b49e32000
Prashanth <imprash4u@gmail.com>
2021-09-07 18:47:21 +0530

    Added: program to find max in 3 integers

--- src/main/java/com/bridgelabz/maximum/Maximum.java
+++ src/main/java/com/bridgelabz/maximum/Maximum.java
@@ -2,12 +2,15 @@ package com.bridgelabz.maximum;
 
 /**
  * Hello world!
- *
  */
-public class Maximum
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Find Maximum in 3 values Using Generics" );
+public class Maximum {
+    public static Integer getmaximum(Integer value1, Integer value2, Integer value3) {
+        Integer max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+        System.out.println(max);
+        return max;
     }
 }
--- src/test/java/com/bridgelabz/maximum/MaximumTest.java
+++ src/test/java/com/bridgelabz/maximum/MaximumTest.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.maximum;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,12 +10,15 @@ import org.junit.Test;
  */
 public class MaximumTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void intMax1() {
+        int actualValue = Maximum.getmaximum(12,14,15);
+        Assert.assertEquals(15,actualValue);
+    }
+
+    @Test
+    public void intMax2() {
+        int actualValue = Maximum.getmaximum(1000,545,746);
+        Assert.assertEquals(1000,actualValue);
     }
 }
