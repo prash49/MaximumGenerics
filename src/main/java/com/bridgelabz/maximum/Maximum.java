ef0fcf3f8d8b2e1b9ee60422badb25739623ab63
Prashanth <imprash4u@gmail.com>
2021-09-07 19:37:12 +0530

    Added:toPrint method in testMaximum Method

--- src/main/java/com/bridgelabz/maximum/Maximum.java
+++ src/main/java/com/bridgelabz/maximum/Maximum.java
@@ -4,16 +4,17 @@ package com.bridgelabz.maximum;
  * Hello world!
  */
 public class Maximum<E extends Comparable> {
-    E value1,value2,value3;
+    E value1, value2, value3;
 
     public Maximum(E value1, E value2, E value3) {
         this.value1 = value1;
         this.value2 = value2;
         this.value3 = value3;
     }
-    public E testMaximum(){
-        E testMax= testMaximum(value1,value2,value3);
-        return  testMax;
+
+    public E testMaximum() {
+        E testMax = testMaximum(value1, value2, value3);
+        return testMax;
     }
 
     public static <E extends Comparable> E testMaximum(E value1, E value2, E value3) {
@@ -22,7 +23,11 @@ public class Maximum<E extends Comparable> {
             max = value2;
         else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
             max = value3;
-        System.out.println(max);
+        toPrint(value1, value2, value3, max);
         return max;
+    }
+
+    private static <E> void toPrint(E value1, E value2, E value3, E max) {
+        System.out.println("Max of "+value1+" , "+value2+" , "+value3+" is: "+max);
     }
 }
